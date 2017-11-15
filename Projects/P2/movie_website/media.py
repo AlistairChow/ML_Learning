0b946f92d034727cd754cb065b48078966f540e4
Alistair Chow <alistair@yeah.net>
2017-11-15 11:46:43 +0800

    Add P2

---
+++ Projects/P2/movie_website/media.py
@@ -0,0 +1,6 @@
+class Movie():
+    def __init__(self, movie_title, movie_stroyline, poster_image_url, trailer_url):
+        self.title = movie_title
+        self.storyline = movie_stroyline
+        self.poster_image_url = poster_image_url
+        self.trailer_url = trailer_url
